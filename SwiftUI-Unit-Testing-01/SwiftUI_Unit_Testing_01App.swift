03c2da1a0e995a11791008e245a8b4d711677719
Mike Panitz <michpa@hotmail.com>
2021-04-18 16:57:09 -0700

    Initial Commit

---
+++ SwiftUI-Unit-Testing-01/SwiftUI_Unit_Testing_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Unit_Testing_01App.swift
+//  SwiftUI-Unit-Testing-01
+//
+//  Created by Mike Panitz on 4/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Unit_Testing_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
